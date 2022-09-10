3fd693ea11fd3b13d2b9ea515288a7e24efe5b54
Aditya <adityasingh220598@gmail.com>
2022-09-10 14:24:23 +0530

    Handling errors

--- app.js
+++ app.js
@@ -43,6 +43,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    customFormatErrorFn(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || "An error occurred.";
+      const code = err.originalError.statusCode || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
